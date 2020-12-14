c012102057610d0ceb1f19c90b88024faaa9a5e1
JOCOghub <jojocohen57@gmail.com>
2020-12-14 14:00:06 -0500

    adds museum action

---
+++ museum/src/actions/museum_actions.js
@@ -0,0 +1,42 @@
+export const fetchPaintings = () => {
+    return(dispatch) => {
+      return fetch('http://localhost:3000/pets')
+      .then(resp => resp.json())
+      .then(pets => {
+        dispatch({ type: "SET_PETS", payload: pets })
+      })
+    }
+  }
+  
+  
+  export const addPainting = pet => {
+    return(dispatch) => {
+      return fetch('http://localhost:3000/pets', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({pet:pet})
+      })
+      .then(resp => resp.json())
+      .then(pet => {
+        dispatch({ type: "ADD_PET", payload: pet })
+      })
+    } 
+  }
+  
+  export const removePainting = petId => {
+    return(dispatch) => {
+      return fetch(`http://localhost:3000/pets/${petId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(resp => resp.json())
+      .then(pet => {
+        dispatch({ type: "REMOVE_PET", payload: petId })
+      })
+    } 
+  }
+  
---
+++ museum/src/actions/painting_actions.js
@@ -0,0 +1,41 @@
+export const fetchMuseums = () => {
+    return(dispatch) => {
+      return fetch('http://localhost:3000/museums')
+      .then(resp => resp.json())
+      .then(museums => {
+        dispatch({ type: "SET_MUSEUMS", payload: museums })
+      })
+    }
+  }
+  //check for bugs
+  
+  export const addMuseum = museum => {
+    return(dispatch) => {
+      return fetch('http://localhost:3000/museums', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({museum:museum})
+      })
+      .then(resp => resp.json())
+      .then(museum => {
+        dispatch({ type: "ADD_MUSEUM", payload: museum })
+      })
+    } 
+  }
+  
+  export const removeMuseum = museumId => {
+    return(dispatch) => {
+      return fetch(`http://localhost:3000/museums/${museumId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(resp => resp.json())
+      .then(museum => {
+        dispatch({ type: "REMOVE_MUSEUM", payload: museumId })
+      })
+    } 
+  }
