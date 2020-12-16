72e51f6afa54f3f1b8d66466244508c7a8f1f2e6
JOCOghub <jojocohen57@gmail.com>
2020-12-15 21:18:00 -0500

    adds container Shows

---
+++ museum/src/containers/MuseumShow.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export class MuseumShow extends Component {
+  constructor(props) {
+    super(props);
+
+    let paramsId = parseInt(this.props.match.params.id, 10)
+    let museum = props.museums.find( museum => museum.id === paramsId);
+
+    this.state = {
+      museum: museum ? museum : { name: 'N/A'}
+    }
+  }
+
+
+  render() {
+    const { name } = this.state.museum;
+    return (
+      <div>
+        <h3>{ name }</h3>
+      </div>
+    )
+  }
+}
+
+export default MuseumShow
